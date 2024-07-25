dff9bbd39ad22ccb977a4e80a97a01ddf60824d4
Mohammed Waseem Khan <waseemp20834@gmail.com>
2024-07-25 21:49:56 +0530

    Add files via upload

--- Chapter-3/03_escape_sqn.py
+++ Chapter-3/03_escape_sqn.py
@@ -1,1 +1,11 @@
-print("hello world") 
+a ="Harry is a good boy\n but not a bad boy" # \n is used to start a new line in a sentence
+
+print(a)
+
+b = "Harry is not a girl\n He is a \"boy\"" #\"boy\" it used to print any words along with the double quotes
+
+print(b)
+
+c ="Harry is a good boy\t but not a bad boy" #Used for tab spacing
+
+print(c)
